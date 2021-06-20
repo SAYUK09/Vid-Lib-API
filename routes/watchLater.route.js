61bb8ea89a80b1ab0227ac1984b5ed14b077bdc9
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-06-20 13:14:31 +0000

    auth and user management in wishlist

--- routes/watchLater.route.js
+++ routes/watchLater.route.js
@@ -1,52 +1,54 @@
 const express = require('express');
 const router = express.Router();
-
+const verify = require("../middlewares/verifyToken")
 const WatchLaterVideo = require("../models/watchLater.model")
 
-router.get('/', async(req ,res)=>{
+router.get('/', verify, async (req, res) => {
 
-  try{
-    const vid = await WatchLaterVideo.find()
+  try {
+    const vid = await WatchLaterVideo.find({ user: req.user._id })
     res.json(vid)
-  }catch(err){
+  } catch (err) {
     console.log("err", err)
   }
 
 });
 
 
-router.post('/', async (req, res)=>{
+router.post('/', verify, async (req, res) => {
   console.log(req.body)
 
   const addVid = req.body
-    const newVideo = new WatchLaterVideo(
-      addVid
-      
-    )
-  
-    try {
-      const vidData = await newVideo.save();
-      res.json(vidData);
-    } catch (error) {
-      res.status(400).json({ success: false, message: error });
+  const newVideo = new WatchLaterVideo(
+    {
+      ...addVid,
+      user: req.user._id
     }
+  )
+
+  try {
+    const vidData = await newVideo.save({user: req.user._id});
+    res.json(vidData);
+  } catch (error) {
+    res.status(400).json({ success: false, message: error });
+  }
 
 })
 
-router.delete('/:vidId', async(req, res)=>{
-  try{
-    const removedPrd = await WatchLaterVideo.remove({_id:req.params.vidId})
-    
-    const newVid = await WatchLaterVideo.find();
+router.delete('/:vidId', async (req, res) => {
+  try {
+    const removedPrd = await WatchLaterVideo.remove({ _id: req.params.vidId,user: req.user._id })
+
+    const newVid = await WatchLaterVideo.find({ user: req.user._id });
     res.json(newVid);
-    
+
   }
-  
-  catch(err){
-    res.json({message:err})
+
+  catch (err) {
+    res.json({ message: err })
     console.log(err)
   }
-  
+
 })
 
 module.exports = router
